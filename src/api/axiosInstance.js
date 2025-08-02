02cd72da8e18621356958307632f9a191044e4d3
Mohammed Safil S <mohammedsafil.s2023cse@sece.ac.in>
2025-08-02 20:24:17 +0530

    6

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://assignmenttracker-rvq9.onrender.com/',
 });
 
 instance.interceptors.request.use(config => {
